cc6ab5a1304d5f8fe7864c982b4cd713ef639a1a
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-12-30 15:46:49 -0300

    Add around advice for logging method execution in GreetingAspect

--- src/main/java/com/antonio/course/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/antonio/course/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,9 +3,11 @@ package com.antonio.course.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -47,5 +49,24 @@ public class GreetingAspect {
     String method = joinPoint.getSignature().getName();
     String args = Arrays.toString(joinPoint.getArgs());
     logger.info("After exception method: " + method + ", args: " + args);
+  }
+
+  @Around("execution(String com.antonio.course.springboot.app.aop.springboot_aop.services.*.*(..))")
+  public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+    String method = joinPoint.getSignature().getName();
+    String args = Arrays.toString(joinPoint.getArgs());
+
+    Object result = null;
+
+    try {
+      logger.info("Before method: " + method + ", args: " + args);
+      result = joinPoint.proceed();
+      logger.info("After method: " + method + ", args: " + args);
+      return result;
+    } catch (Throwable e) {
+      logger.info("After exception method: " + method + ", args: " + args);
+      throw e;
+    }
+
   }
 }
